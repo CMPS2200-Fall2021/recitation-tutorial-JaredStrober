5de8aeacb1935941997702326f057fdf80c2c282
JaredStrober <Jaredstrober19@gmail.com>
2021-08-25 17:08:46 -0500

    Implement Sum of Squares

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+
 def sum_of_squares(a):
-	pass
+    return sum([x**2 for x in a])
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+    assert ([1,2,3,4,5]) == 55
+
+if __name__ == '__main__':
+    test_one()
+    test_two()
