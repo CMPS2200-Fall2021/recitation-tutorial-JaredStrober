92c4378403d039108e45028bbf40582cf930d7be
JaredStrober <Jaredstrober19@gmail.com>
2021-10-01 13:23:25 -0500

    "Finished and implemented the problems! but had a few problems with formatting! Hopefully those are fixed"

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
 def sum_of_squares(a):
-    return sum([x**2 for x in a])
+    return sum([x ** 2 for x in a])
+
 
 def test_one():
-    assert sum_of_squares([1,2,3]) == 14
+    assert sum_of_squares([1, 2, 3]) == 14
+
 
 def test_two():
-    assert ([1,2,3,4,5]) == 55
+    assert ([1, 2, 3, 4, 5]) == 55
+
 
 if __name__ == '__main__':
     test_one()
